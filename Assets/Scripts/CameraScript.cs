7f55f76d1807060750a68cd5215963f01a519a11
Paul Pung <pungpaul2@gmail.com>
2019-10-27 19:10:57 +0200

    Added camera movement and zoom in/out

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    Vector3 lastDragPosition;
+
+    float minFov = 15f;
+    float maxFov = 90f;
+    float sensitivity = 10f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            lastDragPosition = Input.mousePosition;
+        if (Input.GetMouseButton(1))
+        {
+            var delta = lastDragPosition - Input.mousePosition;
+            transform.Translate(delta * Time.deltaTime * 0.75f);
+            lastDragPosition = Input.mousePosition;
+        }
+
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
